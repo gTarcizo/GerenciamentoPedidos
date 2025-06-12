d15341dd8ab26006d61fb440e61ccf7c4247e6aa
Tarcizo <90656673+gTarcizo@users.noreply.github.com>
2025-06-12 13:58:16 -0300

    refator: refatorando campos no banco

--- ProductsAPI/Migrations/20250612165720_RefatorandoBanco.Designer.cs
+++ ProductsAPI/Migrations/20250612165720_RefatorandoBanco.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductsAPI.Infra.Data;
 
@@ -11,9 +12,11 @@ using ProductsAPI.Infra.Data;
 namespace ProductsAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250612165720_RefatorandoBanco")]
+    partial class RefatorandoBanco
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

baa226e98b695f714f62589f2146d6abd0e0a926
Tarcizo <90656673+gTarcizo@users.noreply.github.com>
2025-06-12 15:13:26 -0300

    refator: removendo Dapper

--- ProductsAPI/Infra/Data/AppDbContext.cs
+++ ProductsAPI/Infra/Data/AppDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProductsAPI.Domain;
-using static Dapper.SqlMapper;
 
 namespace ProductsAPI.Infra.Data;
 
